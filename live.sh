14e05d799f3d118cea4b705522dc4fe0bf9985fa
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2020-01-29 08:43:49 -0800

    Add $BASEDIR to live.sh

--- live.sh
+++ live.sh
@@ -1,32 +1,34 @@
-rm -r ejsout/*
-rm -r ejsout-min/*
+BASEDIR=$(dirname "$0")
+echo "$BASEDIR"
 
-cp -r img/ ejsout/img
-cp -r js/ ejsout/js
-cp -r scss/ ejsout/scss
-cp -r fonts/ ejsout/fonts
-cp -r vendors/ ejsout/vendors
-cp -r css/ ejsout/css 
+rm -r $BASEDIR/ejsout/*
+rm -r $BASEDIR/ejsout-min/*
 
-ejs-cli -f index.ejs -o ejsout/
-ejs-cli -f privacy.ejs -o ejsout/
-ejs-cli -f tos.ejs -o ejsout/
-ejs-cli -f elements/index.ejs -o ejsout/
-ejs-cli -f pricing/index.ejs -o ejsout/
+cp -r img/ $BASEDIR/ejsout/img
+cp -r js/ $BASEDIR/ejsout/js
+cp -r scss/ $BASEDIR/ejsout/scss
+cp -r fonts/ $BASEDIR/ejsout/fonts
+cp -r vendors/ $BASEDIR/ejsout/vendors
+cp -r css/ $BASEDIR/ejsout/css 
+cp -r 404.html $BASEDIR/ejsout/404.html
+
+ejs-cli -f index.ejs -o $BASEDIR/ejsout/
+ejs-cli -f privacy.ejs -o $BASEDIR/ejsout/
+ejs-cli -f tos.ejs -o $BASEDIR/ejsout/
+ejs-cli -f elements/index.ejs -o $BASEDIR/ejsout/
+ejs-cli -f pricing/index.ejs -o $BASEDIR/ejsout/
 
 echo "Minifying html"
 
-html-minifier --collapse-whitespace --remove-comments --minify-css true --minify-js true --minify-urls true --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --input-dir ejsout/ --output-dir ejsout-min/
+html-minifier --collapse-whitespace --remove-comments --minify-css true --minify-js true --minify-urls true --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --input-dir $BASEDIR/ejsout/ --output-dir $BASEDIR/ejsout-min/
 echo "Copying residue files"
-cp -r ejsout/css/* ejsout-min/css/
-cp -r ejsout/js/* ejsout-min/js/
-cp -r ejsout/img/* ejsout-min/img/
-cp -r ejsout/scss/* ejsout-min/scss/
-cp -r ejsout/fonts/* ejsout-min/fonts/
-cp -r ejsout/vendors/* ejsout-min/vendors/
+cp -r $BASEDIR/ejsout/css/* $BASEDIR/ejsout-min/css/
+cp -r $BASEDIR/ejsout/js/* $BASEDIR/ejsout-min/js/
+cp -r $BASEDIR/ejsout/img/* $BASEDIR/ejsout-min/img/
+cp -r $BASEDIR/ejsout/scss/* $BASEDIR/ejsout-min/scss/
+cp -r $BASEDIR/ejsout/fonts/* $BASEDIR/ejsout-min/fonts/
+cp -r $BASEDIR/ejsout/vendors/* $BASEDIR/ejsout-min/vendors/
 
 echo "Pushing to Google now..."
 
-cd ejsout-min/
-
-gsutil -m rsync -r $(pwd) gs://evaapp.xyz/
+gsutil -m rsync -r $BASEDIR/ejsout-min gs://evaapp.xyz/
